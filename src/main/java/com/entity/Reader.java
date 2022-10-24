b4fcf74143c19b62575d774b1337a7ec50b7f7f9
Giang <doantragiang.nguyen@my.jcu.edu.au>
2022-10-24 17:06:00 +0700

    Add initial data into Reader entity

---
+++ src/main/java/com/entity/Reader.java
@@ -0,0 +1,30 @@
+package com.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reader")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Reader {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int readerId;
+
+    @Column(name="first_name", nullable = false, length = 30)
+    private String firstName;
+
+    @Column(name="last_name", nullable = false, length = 30)
+    private String lastName;
+
+    @Column(name="phone_no", nullable = false)
+    private int phoneNum;
+
+    @Column(name="email")
+    private String email;
+}
