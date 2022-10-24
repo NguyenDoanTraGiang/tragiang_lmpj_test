ac78d930b06544ad4d590e1c227e309f59f3261f
Giang <doantragiang.nguyen@my.jcu.edu.au>
2022-10-24 17:05:53 +0700

    Add initial data into Book entity

---
+++ src/main/java/com/entity/Book.java
@@ -0,0 +1,31 @@
+package com.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookId;
+
+    @Column(name="book_name", nullable = false, length=100)
+    private String bookName;
+
+    @Column(name="author_name", nullable = false, length=100)
+    private String authorName;
+
+    @Column(name="edition", length = 5)
+    private String edition;
+
+    @Column(name="status", nullable = false)
+    private boolean status;
+
+}
