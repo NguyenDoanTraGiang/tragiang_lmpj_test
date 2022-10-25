cc4d27ced7607e05d52775fdcfc9721ad78af68c
Giang <doantragiang.nguyen@my.jcu.edu.au>
2022-10-25 10:10:54 +0700

    Add getReaderList()

--- src/main/java/com/controller/ReaderController.java
+++ src/main/java/com/controller/ReaderController.java
@@ -1,10 +1,16 @@
 package com.controller;
 
+import com.dto.ReaderDto;
+import com.entity.Reader;
 import com.service.ReaderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/library")
@@ -16,6 +22,18 @@ public class ReaderController {
     ReaderController(ReaderService readerService){
         super();
         this.readerService = readerService;
+    }
+
+    /**
+     * Xem danh sach cua tat ca nguoi muon
+     * @return
+     */
+
+    @GetMapping("/readers")
+    public List<ReaderDto> getReaderList(){
+        List<Reader> readerList = readerService.getReaderList();
+        List<ReaderDto> readerDtoList = readerList.stream().map(reader -> modelMapper.map(reader, ReaderDto.class)).collect(Collectors.toList());
+        return readerDtoList;
     }
 
 
