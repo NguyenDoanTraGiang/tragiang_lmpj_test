63675c037b521a4fb2e647cab0f7c4443007b34c
Giang <doantragiang.nguyen@my.jcu.edu.au>
2022-10-26 17:29:44 +0700

    GIANGLMPJ-14: change return data type for getBookList()

--- src/main/java/com/repository/BookRepository.java
+++ src/main/java/com/repository/BookRepository.java
@@ -1,5 +1,6 @@
 package com.repository;
 
+import com.dto.BookDetailsDto;
 import com.entity.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,8 +14,8 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findAll();
 
     @Query(value = "SELECT book.book_id, book.book_name, book.author_name, book.edition, book.status, book_type.type_name as bookTypeName" +
-            "FROM book LEFT JOIN book_type ON book.book_type_id = book_type.type_id", nativeQuery = true)
-    Book getBookInfo(Long id);
+            "FROM book JOIN book_type ON book.book_type_id = book_type.type_id", nativeQuery = true)
+    List<Book> getBookList();
 
     Book getBookById(Long id);
 }
