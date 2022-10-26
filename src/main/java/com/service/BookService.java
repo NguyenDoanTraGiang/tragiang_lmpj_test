dd5244a22c42ffd8a6ca41b94c186af9580c0f36
Giang <doantragiang.nguyen@my.jcu.edu.au>
2022-10-26 17:28:29 +0700

    GIANGLMPJ-14: Delete unused methods getBookInfo

--- src/main/java/com/service/BookService.java
+++ src/main/java/com/service/BookService.java
@@ -1,14 +1,11 @@
 package com.service;
 
 import com.entity.Book;
-import com.repository.BookRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BookService {
-    Book getBookInfo(Long bookId);
     Book getBookById(Long bookId);
     List<Book> getBookList();
     Book createBook(Book book);
