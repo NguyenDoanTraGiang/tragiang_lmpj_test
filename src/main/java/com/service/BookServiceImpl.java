c23844165613b687654b7d3eeca0fba9da2cb7fb
Giang <doantragiang.nguyen@my.jcu.edu.au>
2022-10-26 17:30:27 +0700

    GIANGLMPJ-14: delete unused methods getBookInfo

--- src/main/java/com/service/BookServiceImpl.java
+++ src/main/java/com/service/BookServiceImpl.java
@@ -2,7 +2,6 @@ package com.service;
 
 import com.entity.Book;
 import com.repository.BookRepository;
-import com.repository.ReaderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +14,6 @@ public class BookServiceImpl implements BookService{
     @Autowired
     private BookRepository bookRepository;
 
-    @Override
-    public Book getBookInfo(Long bookId){
-        return bookRepository.getBookInfo(bookId);
-    }
 
     @Override
     public Book getBookById(Long bookId){
